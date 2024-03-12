4b5e66b1eddd1f58b19a09b8ceec106f80e99c02
Michael <88445485+mjnyland@users.noreply.github.com>
2024-03-12 08:23:59 -0400

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Michael Nyland on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
